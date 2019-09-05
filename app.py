ac85bfa7f4181b600ff46c84900a166cf33be672
Cole Garien <colegarien@gmail.com>
2019-09-04 21:10:14 -0500

    make event based in preparation for event queueing

--- app.py
+++ app.py
@@ -7,61 +7,97 @@ app.config.from_pyfile('ozzytron.cfg', silent=True)
 
 repo = Repo()
 
+class Event:
+    def fromRequest(request):
+        eventData = request.get('event', {})
+        event = Event(eventData.get('user',''),eventData.get('channel',''),eventData.get('text',''),eventData.get('type', 'none'),eventData.get('channel_type','normal'))
+        return event
+    
+    def __init__(self, user, channel, text, eventType, channelType):
+        self.sourceUser = user
+        self.sourceChannel = channel
+        self.text = text
+        self.eventType = eventType
+        self.channelType = channelType
+
+    def isEmpty(self):
+        return self.eventType == 'none'
+
+    def isMentionType(self):
+        return self.eventType == 'app_mention'
+        
+    def isPrivateMessageType(self):
+        return self.eventType == 'message' and self.channelType == 'im'
+
+class EventHandler:
+    def isMatch(event : Event):
+        return False
+    def handle(event : Event):
+        pass
+
+class MakeMeMan(EventHandler):
+    def isMatch(event : Event):
+        return event.isPrivateMessageType() and event.text.find('make me a man') > -1
+    def handle(event : Event):
+        player = Player()
+        player.username = event.sourceUser
+        repo.savePlayer(player)
+
+class AmIMan(EventHandler):
+    def isMatch(event : Event):
+        return event.isMentionType() and event.text.find('am I a man?') > -1
+    def handle(event : Event):
+        current = repo.getPlayerForUser(event.sourceUser)
+        if current == None:
+            message = 'No <@'+event.sourceUser+'>. No you are not.'
+        else:
+            message = 'Yes <@'+event.sourceUser+'>! You are man ' + str(current.id) + '. Your wallet currently has '+str(current.ozzyTokens)+' OTs' 
+        postToChat(event.sourceChannel, message)
+
+class CallSomebody(EventHandler):
+    def isMatch(event : Event):
+        return event.isMentionType() and event.text.find('call') > -1
+    def handle(event : Event):
+        message = '<@' + event.sourceUser + '>, Call who a what now?'
+        callIndex = event.text.find('call')
+        userStart = event.text.find('<@', callIndex)
+        userEnd = event.text.find('>', callIndex)
+        aIndex = event.text.find(' a ', userEnd)
+        if userStart > -1 and userEnd > -1 and aIndex > -1:
+            target = event.text[userStart:userEnd+1].strip()
+            what = event.text[aIndex+2:].strip()
+            if len(target) > 0 and target == '<@'+event.sourceUser+'>':
+                message = 'Sorry <@'+event.sourceUser+'>, that\'s fucked up.' 
+            elif len(what) > 0 and len(target) > 0:
+                message = target + ', you a ' + what + '.'
+        postToChat(event.sourceChannel, message)
+
+eventHandlers = ['MakeMeMan', 'AmIMan', 'CallSomebody']
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     content = request.get_json()
     if content != None and content['type'] == 'url_verification':
         return jsonify({'challenge' : content['challenge']})
-    elif content != None and content['event']['type'] == 'app_mention':
-       handleMention(content) 
-    elif content != None and content['event']['type'] == 'message' and content['event']['channel_type'] == 'im':
-        handlePrivateMessage(content)
     elif content != None:
-        print(content['event']['type'])
+        processEvent(eventHandlers, Event.fromRequest(content))
 
     return jsonify({'success':True})
-
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error':'Not found'}), 404)
 
-def handlePrivateMessage(content):
-    user = content['event']['user']
-    channel = content['event']['channel']
-    text = content['event']['text']
-    if text.find('make me a man') > -1:
-        player = Player()
-        player.username = user
-        repo.savePlayer(player)
 
-
-def handleMention(content):
-    user = content['event']['user']
-    channel = content['event']['channel']
-    text = content['event']['text']
-    if text.find('am I a man?') > -1:
-        current = repo.getPlayerForUser(user)
-        if current == None:
-            message = 'No <@'+user+'>. No you are not.'
-        else:
-            message = 'Yes <@'+user+'>! You are man ' + str(current.id) + '. Your wallet currently has '+str(current.ozzyTokens)+' OTs' 
-    elif text.find('call') > -1:
-        message = '<@' + user + '>, Call who a what now?'
-        callIndex = text.find('call')
-        userStart = text.find('<@', callIndex)
-        userEnd = text.find('>', callIndex)
-        aIndex = text.find(' a ', userEnd)
-        if userStart > -1 and userEnd > -1 and aIndex > -1:
-            target = text[userStart:userEnd+1].strip()
-            what = text[aIndex+2:].strip()
-            if len(target) > 0 and target == '<@'+user+'>':
-                message = 'Sorry <@'+user+'>, that\'s fucked up.' 
-            elif len(what) > 0 and len(target) > 0:
-                message = target + ', you a ' + what + '.'
-    else:
-        message = 'Sorry <@' + user + '>, I\'mma damn retard!'
-    postToChat(channel, message)
+def processEvent(handlers, event):
+    for handlerClass in handlers:
+        handler = globals()[handlerClass]
+        if getattr(handler, 'isMatch')(event):
+            getattr(handler, 'handle')(event)
+            return True
+    message = 'Sorry <@' + event.sourceUser + '>, I\'mma damn retard!'
+    postToChat(event.sourceChannel, message)
+    return False
 
 def getSlackApiHeaders():
     return {'Content-Type' : 'application/json; charset=utf-8', 'Authorization' : 'Bearer ' + app.config['BOT_OAUTH'] }
